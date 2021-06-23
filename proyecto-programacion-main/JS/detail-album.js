9cfdf1e7cf23650d6da4554cdcfdcbbc8a27da50
MiltonKienigiel <miltonkienigiel@gmail.com>
2021-06-23 04:02:12 -0300

    Detalle artista y detalle cancion y home

--- proyecto-programacion-main/JS/detail-album.js
+++ proyecto-programacion-main/JS/detail-album.js
@@ -1,5 +1,32 @@
-window.addEventListener('load', function(){
+
+//obtengo el query string
+let queryString = window.location.search
+
+//paso de ese texto a un objeto
+let objetoQuery = new URLSearchParams(queryString);
+
+//ahora si obtengo el id del album
+var albumId = objetoQuery.get('id');
 
 
-    
-})
+
+
+fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/"+albumId)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data);
+        
+        let contenedorData = document.querySelector(".fila1");
+        let album = data
+        console.log(album);
+        
+
+
+
+
+        
+    }).catch(function(error){
+        console.error(error)
+    })
--- proyecto-programacion-main/JS/detail-track.js
+++ proyecto-programacion-main/JS/detail-track.js
@@ -1,5 +1,46 @@
-window.addEventListener('load', function(){
+
+//obtengo el query string
+let queryString = window.location.search
+
+//paso de ese texto a un objeto
+let objetoQuery = new URLSearchParams(queryString);
+
+//ahora si obtengo el id del album
+var trackId = objetoQuery.get('id');
 
 
-    
+
+fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/" +trackId)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+
+
+let contenedorData = document.querySelector(".MainContent")
+let subtitulo = document.querySelector(".Subtitle")
+
+let track = data
+console.log(track);
+subtitulo.innerHTML=`
+    ${track.title}
+`
+contenedorData.innerHTML = ` 
+<article>
+<div class="card">
+  <div class="avatar" style="background-image: url('${track.album.cover_big}')"></div>
+  <div>
+    <h1>${track.title}</h1>
+    <h3 id=infosingle><a href="detail-artist.html?id=${track.artist.id}">${track.artist.name}</a>
+    </h3>
+    <h3 id=infosingle>Type:${track.type}</h3>
+    <h3 id=infosingle>${track.duration} Seg </h3>
+
+    <button id=addplaylist href=""> Add to Playlist</button>
+    <h4> <a href="playlist.html"  class="formap"> Mi Playlist</a> </h4>
+    <audio id=audios src="${track.preview}" controls="audios/muchacha-almendra.mp3"> </audio>
+  </div>
+</div>
+</article>`
+
 })
